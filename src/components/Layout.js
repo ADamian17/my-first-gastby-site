00ff6f657239aaf043d276d82e7fd3b87a4b105a
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-18 12:20:11 -0700

    part 4
    Signed-off-by: ADamian17 <adonisdesuvikdo@gmail.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,31 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
+import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import { 
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText  
+  navLinkText,
+  siteTitle  
 } from "./Layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+  `);
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+
       <nav>
         <ul className={navLinks}>
 
@@ -22,6 +35,12 @@ const Layout = ({ pageTitle, children }) => {
 
           <li className={navLinkItem}>
             <Link className={navLinkText} to="/about">About</Link>
+          </li>
+
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
           </li>
 
         </ul>
