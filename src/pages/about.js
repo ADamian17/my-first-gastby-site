fad3ece54bf71447fefd711f9a6dda674c66756e
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-18 09:54:06 -0700

    added first index, about and layout components
    Signed-off-by: ADamian17 <adonisdesuvikdo@gmail.com>

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby';
+
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Page">
+    <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
